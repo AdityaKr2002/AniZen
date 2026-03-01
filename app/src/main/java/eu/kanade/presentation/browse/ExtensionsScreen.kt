b37f3dc90533ad7839ac2f7141b31b00f49d22ea
salmanbappi <salmanbappi@users.noreply.github.com>
2026-03-01 19:23:29 +0600

    Fix: missing background import in ExtensionsScreen.kt

--- app/src/main/java/eu/kanade/presentation/browse/ExtensionsScreen.kt
+++ app/src/main/java/eu/kanade/presentation/browse/ExtensionsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.material3.Surface
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
