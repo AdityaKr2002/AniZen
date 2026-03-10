88d42d2c75f220a243bac9991009c55454816eb2
salmanbappi <salmanbappi@users.noreply.github.com>
2026-03-10 06:28:42 +0600

    style: replace remaining old branding icons in notifications and player popups

--- app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateNotifier.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateNotifier.kt
@@ -211,7 +211,7 @@ internal class AppUpdateNotifier(private val context: Context) {
         with(notificationBuilder) {
             setContentTitle(context.stringResource(KMR.strings.update_completed))
             setContentText(context.stringResource(MR.strings.updated_version, BuildConfig.VERSION_NAME))
-            setSmallIcon(R.drawable.ic_komikku)
+            setSmallIcon(R.mipmap.ic_launcher)
             setAutoCancel(true)
             setOngoing(false)
             setProgress(0, 0, false)
