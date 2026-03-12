5986ec99aede68be0cd607c21f140eb9cb6dc283
salmanbappi <salmanbappi@users.noreply.github.com>
2026-03-12 20:34:08 +0600

    Restore accidentally deleted domain files and refine migration interactor location

---
+++ domain/src/main/java/mihon/domain/extensionrepo/service/ExtensionRepoDto.kt
@@ -0,0 +1,33 @@
+package mihon.domain.extensionrepo.service
+
+import kotlinx.serialization.Serializable
+import mihon.domain.extensionrepo.model.ExtensionRepo
+
+@Serializable
+data class ExtensionRepoMetaDto(
+    val meta: ExtensionRepoDto,
+)
+
+@Serializable
+data class ExtensionRepoDto(
+    val name: String,
+    val shortName: String?,
+    val website: String,
+    val signingKeyFingerprint: String,
+    val author: String? = null,
+)
+
+fun ExtensionRepoMetaDto.toExtensionRepo(
+    baseUrl: String,
+    author: String? = null,
+): ExtensionRepo {
+    return ExtensionRepo(
+        baseUrl = baseUrl,
+        name = meta.name,
+        shortName = meta.shortName,
+        website = meta.website,
+        signingKeyFingerprint = meta.signingKeyFingerprint,
+        isVisible = true,
+        author = author ?: meta.author,
+    )
+}
