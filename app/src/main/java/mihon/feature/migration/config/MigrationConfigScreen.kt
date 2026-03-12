33090da5c4eb833c4205b6d9cbb1ad5a9e64b88b
salmanbappi <salmanbappi@users.noreply.github.com>
2026-03-13 05:25:56 +0600

    Fix build: resolve correct function name in LocaleHelper for MigrationConfigScreen

--- app/src/main/java/mihon/feature/migration/config/MigrationConfigScreen.kt
+++ app/src/main/java/mihon/feature/migration/config/MigrationConfigScreen.kt
@@ -57,7 +57,7 @@ import eu.kanade.presentation.components.AppBarActions
 import eu.kanade.presentation.util.Screen
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.system.LocaleHelper
-import eu.kanade.tachiyomi.util.system.LocaleHelper.getShortDisplayName
+import eu.kanade.tachiyomi.util.system.LocaleHelper.getDisplayName
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.update
 import mihon.feature.migration.list.MigrationListScreen
@@ -464,7 +464,7 @@ class MigrationConfigScreen(private val animeIds: Collection<Long>) : Screen() {
     data class MigrationSource(
         val source: Source,
         val isSelected: Boolean,
-        val shortLanguage: String = getShortDisplayName(source.lang),
+        val shortLanguage: String = getDisplayName(source.lang),
         val upperShortLanguage: String = shortLanguage.uppercase(),
     ) {
         val id: Long
