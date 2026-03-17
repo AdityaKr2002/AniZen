3c235edbe8c58fe5125fa2031aa85d5c74807c45
salmanbappi <salmanbappi@users.noreply.github.com>
2026-03-17 19:56:57 +0600

    fix: add missing systemBars import in AnimeToolbar

--- app/src/main/java/eu/kanade/presentation/anime/components/AnimeToolbar.kt
+++ app/src/main/java/eu/kanade/presentation/anime/components/AnimeToolbar.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Download
