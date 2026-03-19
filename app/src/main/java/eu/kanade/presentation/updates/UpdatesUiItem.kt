69bc7b5bded725fa063be594704d44a43f805d99
salmanbappi <salmanbappi@users.noreply.github.com>
2026-03-20 05:26:24 +0600

    fix(ui): cleanup imports and use fully qualified names for State to resolve clashes

--- app/src/main/java/eu/kanade/presentation/updates/UpdatesUiItem.kt
+++ app/src/main/java/eu/kanade/presentation/updates/UpdatesUiItem.kt
@@ -34,7 +34,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
