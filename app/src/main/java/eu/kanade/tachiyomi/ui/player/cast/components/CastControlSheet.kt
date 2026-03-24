827125e4bf4a9338024840f1d02854d017835d8e
salmanbappi <salmanbappi@users.noreply.github.com>
2026-03-25 04:35:49 +0600

    chore: fix missing itemsIndexed imports

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/cast/components/CastControlSheet.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/cast/components/CastControlSheet.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FilledTonalButton
