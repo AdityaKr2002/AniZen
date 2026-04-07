59ca89f9c1499223bb38c3e4dc6c635536c7320e
salmanbappi <salmanbappi@users.noreply.github.com>
2026-04-07 14:01:04 +0600

    Revert "Fix: WheelPicker off-by-one initial selection bug due to content padding alignment"
    This reverts commit e7ddf3dbb6879e2719adf2a234bf0eac9d7a6b12.

--- presentation-core/src/main/java/tachiyomi/presentation/core/components/WheelPicker.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/components/WheelPicker.kt
@@ -115,26 +115,7 @@ private fun <T> WheelPicker(
     itemContent: @Composable LazyItemScope.(item: T) -> Unit,
 ) {
     val haptic = LocalHapticFeedback.current
-
-    val density = androidx.compose.ui.platform.LocalDensity.current
-    val itemSizePx = remember(density, size) { with(density) { (size.height / ROW_COUNT).roundToPx() } }
-
-    val initialIndex = remember(startIndex) {
-        if (startIndex == 0) 0
-        else if (startIndex == 1) 0
-        else startIndex - 1
-    }
-    
-    val initialOffset = remember(startIndex, itemSizePx) {
-        if (startIndex == 0) 0
-        else if (startIndex == 1) itemSizePx
-        else 0
-    }
-
-    val lazyListState = rememberLazyListState(
-        initialFirstVisibleItemIndex = initialIndex,
-        initialFirstVisibleItemScrollOffset = initialOffset
-    )
+    val lazyListState = rememberLazyListState(startIndex)
 
     var internalIndex by remember { mutableIntStateOf(startIndex) }
     val internalOnSelectionChanged: (Int) -> Unit = {
