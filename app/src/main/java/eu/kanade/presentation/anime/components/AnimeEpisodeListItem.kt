ca0be927c50b05345c522bdf2fc8bf558c102713
salmanbappi <salmanbappi@users.noreply.github.com>
2026-04-24 03:56:26 +0600

    fix(anime): add missing layout imports for UI stability

--- app/src/main/java/eu/kanade/presentation/anime/components/AnimeEpisodeListItem.kt
+++ app/src/main/java/eu/kanade/presentation/anime/components/AnimeEpisodeListItem.kt
@@ -1,5 +1,6 @@
 package eu.kanade.presentation.anime.components
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
