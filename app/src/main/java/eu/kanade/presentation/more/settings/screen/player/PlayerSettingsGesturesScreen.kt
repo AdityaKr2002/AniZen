158cce3f5ecd71fd262c25c469f1752c52ae7366
salmanbappi <salmanbappi@users.noreply.github.com>
2026-04-27 15:13:44 +0600

    feat(player): add UI toggles for double tap and polish playback timer font

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/player/PlayerSettingsGesturesScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/player/PlayerSettingsGesturesScreen.kt
@@ -40,11 +40,12 @@ object PlayerSettingsGesturesScreen : SearchableSettings {
     @Composable
     override fun getPreferences(): List<Preference> {
         val gesturePreferences = remember { Injekt.get<GesturePreferences>() }
+        val playerPreferences = remember { Injekt.get<eu.kanade.tachiyomi.ui.player.settings.PlayerPreferences>() }
 
         return listOf(
             getSlidersGroup(gesturePreferences = gesturePreferences),
             getSeekingGroup(gesturePreferences = gesturePreferences),
-            getDoubleTapGroup(gesturePreferences = gesturePreferences),
+            getDoubleTapGroup(gesturePreferences = gesturePreferences, playerPreferences = playerPreferences),
             getMediaControlsGroup(gesturePreferences = gesturePreferences),
         )
     }
@@ -134,14 +135,33 @@ object PlayerSettingsGesturesScreen : SearchableSettings {
     }
 
     @Composable
-    private fun getDoubleTapGroup(gesturePreferences: GesturePreferences): Preference.PreferenceGroup {
+    private fun getDoubleTapGroup(
+        gesturePreferences: GesturePreferences,
+        playerPreferences: eu.kanade.tachiyomi.ui.player.settings.PlayerPreferences
+    ): Preference.PreferenceGroup {
         val leftDoubleTap = gesturePreferences.leftDoubleTapGesture()
         val centerDoubleTap = gesturePreferences.centerDoubleTapGesture()
         val rightDoubleTap = gesturePreferences.rightDoubleTapGesture()
 
+        val showDoubleTapOvals = playerPreferences.showDoubleTapOvals()
+        val showSeekIcon = playerPreferences.showSeekIcon()
+        val showSeekTimeWhileSeeking = playerPreferences.showSeekTimeWhileSeeking()
+
         return Preference.PreferenceGroup(
             title = stringResource(MR.strings.pref_category_double_tap),
             preferenceItems = persistentListOf(
+                Preference.PreferenceItem.SwitchPreference(
+                    pref = showDoubleTapOvals,
+                    title = stringResource(MR.strings.pref_show_double_tap_ovals),
+                ),
+                Preference.PreferenceItem.SwitchPreference(
+                    pref = showSeekIcon,
+                    title = stringResource(MR.strings.pref_show_seek_icon),
+                ),
+                Preference.PreferenceItem.SwitchPreference(
+                    pref = showSeekTimeWhileSeeking,
+                    title = stringResource(MR.strings.pref_show_seek_time_while_seeking),
+                ),
                 Preference.PreferenceItem.ListPreference(
                     pref = leftDoubleTap,
                     title = stringResource(MR.strings.pref_left_double_tap),
