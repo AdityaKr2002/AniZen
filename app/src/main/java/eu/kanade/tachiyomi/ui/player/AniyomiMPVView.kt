9df01a9f45c8124516a57b8d009f6d26a3dddc63
salmanbappi <salmanbappi@users.noreply.github.com>
2026-04-29 17:14:56 +0600

    Fix black screen on pause: use keep-open=always
    - Force keep-open to 'always' in AniyomiMPVView initialization to prevent surface reclamation when paused.

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/AniyomiMPVView.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/AniyomiMPVView.kt
@@ -260,8 +260,8 @@ class AniyomiMPVView(context: Context, attributes: AttributeSet) : BaseMPVView(c
 
         MPVLib.setOptionString("msg-level", "all=" + if (networkPreferences.verboseLogging().get()) "v" else "warn")
 
-        MPVLib.setOptionString("keep-open", "always")
         MPVLib.setPropertyBoolean("input-default-bindings", true)
+        MPVLib.setOptionString("keep-open", "always")
 
         MPVLib.setOptionString("tls-verify", "yes")
         MPVLib.setOptionString("tls-ca-file", "${context.filesDir.path}/cacert.pem")
