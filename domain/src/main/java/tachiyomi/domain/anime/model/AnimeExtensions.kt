14111543f468d50dc41c731d7a3a132c19e6a493
salmanbappi <salmanbappi@users.noreply.github.com>
2026-05-08 16:22:01 +0600

    fix: Restore toSAnime and fix domain compilation errors

--- domain/src/main/java/tachiyomi/domain/anime/model/AnimeExtensions.kt
+++ domain/src/main/java/tachiyomi/domain/anime/model/AnimeExtensions.kt
@@ -1,21 +1,35 @@
 package tachiyomi.domain.anime.model
 
-import eu.kanade.tachiyomi.animesource.model.SAnime
-import eu.kanade.tachiyomi.source.model.UpdateStrategy
+import eu.kanade.tachiyomi.animesource.model.SAnime as SAnimeSource
+import eu.kanade.tachiyomi.source.model.SAnime
 
-fun SAnime.toDomainAnime(sourceId: Long): Anime {
+fun Anime.toSAnime(): SAnime = SAnime.create().also {
+    it.url = url
+    it.title = title
+    it.artist = artist
+    it.author = author
+    it.description = description
+    it.genre = genre.orEmpty().joinToString()
+    it.status = status.toInt()
+    it.thumbnail_url = thumbnailUrl
+    it.initialized = initialized
+}
+
+fun SAnimeSource.toDomainAnime(sourceId: Long): Anime {
     return Anime.create().copy(
         url = url,
+        // SY -->
         ogTitle = title,
-        source = sourceId,
         ogArtist = artist,
         ogAuthor = author,
         ogThumbnailUrl = thumbnail_url,
         ogDescription = description,
         ogGenre = genre?.split(", ")?.map { it.trim() },
         ogStatus = status.toLong(),
+        // SY <--
+        updateStrategy = update_strategy,
         initialized = initialized,
-        updateStrategy = UpdateStrategy.ALWAYS_UPDATE, // Default
+        source = sourceId,
     )
 }
 
