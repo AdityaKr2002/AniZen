ad9411e91f898ed7c1e5d33b7754ef07980fef79
salmanbappi <salmanbappi@users.noreply.github.com>
2026-06-30 17:36:19 +0600

    fix(track): fix unresolved reference compile errors in TrackInfoDialog.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/anime/track/TrackInfoDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/anime/track/TrackInfoDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.text.input.rememberTextFieldState
