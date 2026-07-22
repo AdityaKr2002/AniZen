56fcfcfccee00363bf8ead6707269e04db345079
salmanbappi <salmanbappi@users.noreply.github.com>
2026-07-23 01:40:06 +0600

    fix(network): restore GitForgeInterceptor to override saved UA preference for Codeberg/GitLab

--- core/common/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ core/common/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
 import eu.kanade.tachiyomi.network.interceptor.IgnoreGzipInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UncaughtExceptionInterceptor
+import eu.kanade.tachiyomi.network.interceptor.GitForgeInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UserAgentInterceptor
 import okhttp3.Cache
 import okhttp3.ConnectionPool
@@ -84,6 +85,7 @@ open /* SY <-- */ class NetworkHelper(
             )
             .addInterceptor(UncaughtExceptionInterceptor())
             .addInterceptor(UserAgentInterceptor(::defaultUserAgentProvider))
+            .addInterceptor(GitForgeInterceptor)
             .addNetworkInterceptor(IgnoreGzipInterceptor())
             .addNetworkInterceptor(BrotliInterceptor)
 
